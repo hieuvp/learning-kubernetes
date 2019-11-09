e34b1ad657b2de974734a6b1ec89c04ecac5d81c
Harrison Van <harrison.van@shopback.com>
2019-11-09 12:22:19 +0700

    docs

--- 06-secrets-and-config-maps/labs/create-configmap.sh
+++ 06-secrets-and-config-maps/labs/create-configmap.sh
@@ -12,11 +12,11 @@
 # @see: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail
 set -eoux pipefail
 # -e: any command returning a non-zero exit code will cause an immediate exit
-# -o: set the exit code of a pipeline (a sequence of commands separated by "|" or "|&")
+# -o: set the exit code of a pipeline (which is a sequence of commands separated by "|" or "|&")
 #     to the rightmost command that exits with a non-zero status,
 #     or to zero if all commands of the pipeline exit successfully
 # -u: cause the bash shell to treat unset variables as an error and exit immediately
 # -x: cause bash to print each command before executing it,
-#     great help when trying to debug a bash script failure
+#     great help when debugging a bash script failure
 
 kubectl create configmap mariadb-config --from-file=labs/max_allowed_packet.cnf
