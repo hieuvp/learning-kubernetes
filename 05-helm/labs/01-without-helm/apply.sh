0d132ca5f95896ce21c73692c1ff2df02089ed7f
Harrison Van <harrison.van@shopback.com>
2019-11-09 23:20:26 +0700

    order

--- 05-helm/labs/01-without-helm/apply.sh
+++ 05-helm/labs/01-without-helm/apply.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
-kubectl apply --filename labs/01-without-helm/frontend.yaml
+# Frontend
 kubectl apply --filename labs/01-without-helm/frontend-config.yaml
+kubectl apply --filename labs/01-without-helm/frontend.yaml
 kubectl apply --filename labs/01-without-helm/frontend-service.yaml
 kubectl apply --filename labs/01-without-helm/ingress.yaml
 
-kubectl apply --filename labs/01-without-helm/backend.yaml
+# Backend API
 kubectl apply --filename labs/01-without-helm/backend-secret.yaml
+kubectl apply --filename labs/01-without-helm/backend.yaml
 kubectl apply --filename labs/01-without-helm/backend-service.yaml
 
-kubectl apply --filename labs/01-without-helm/mongodb.yaml
+# Database
+kubectl apply --filename labs/01-without-helm/mongodb-secret.yaml
 kubectl apply --filename labs/01-without-helm/mongodb-pv.yaml
 kubectl apply --filename labs/01-without-helm/mongodb-pvc.yaml
-kubectl apply --filename labs/01-without-helm/mongodb-secret.yaml
+kubectl apply --filename labs/01-without-helm/mongodb.yaml
 kubectl apply --filename labs/01-without-helm/mongodb-service.yaml
