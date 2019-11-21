a4c612a13b6c21789614cb8620d65e6b8744a015
Harrison Van <harrison.van@shopback.com>
2019-11-21 10:59:03 +0700

    create-certificates.sh

--- 08-rbac-authorization/labs/01-creating-users/create-certificates.sh
+++ 08-rbac-authorization/labs/01-creating-users/create-certificates.sh
@@ -9,29 +9,43 @@ declare -r USERNAME="harrison"
 rm -rf ${OUTPUT_DIR}
 mkdir ${OUTPUT_DIR}
 
-cp ~/.minikube/ca.crt ${OUTPUT_DIR}
-
 # RSA is popular format use to create asymmetric key pairs those named public and private key
-# Generate an RSA Private Key
+# Generate an RSA private key
 openssl genrsa -out ${OUTPUT_DIR}/${USERNAME}.key 2048
+
+# Read your RSA private key
 openssl rsa -in .certificates/${USERNAME}.key -check
 
-# Certificate Sign Request
+# The CSR (or Certificate Signing Request) is created using the PEM format
+# and contains the public key portion of the private key
+# as well as information about you (or your company)
 openssl req -new \
   -key ${OUTPUT_DIR}/${USERNAME}.key \
   -out ${OUTPUT_DIR}/${USERNAME}.csr \
   -subj "/CN=${USERNAME}/O=devs/O=tech-lead"
-cat ${OUTPUT_DIR}/${USERNAME}.csr
 
-# Certificate
+# Read your Certificate Signing Request
+openssl req -text -noout -verify -in ${OUTPUT_DIR}/${USERNAME}.csr
+
+# Certificate Authority (CA)
+# ca.crt: the certificate file
+# ca.key: the RSA private key
+cp ~/.minikube/ca.crt ${OUTPUT_DIR}/
+cp ~/.minikube/ca.key ${OUTPUT_DIR}/
+
+# An X.509 certificate is a digital certificate
+# that uses the widely accepted international X.509 public key infrastructure (PKI) standard
+# to verify that a public key belongs to
+# the user, computer or service identity contained within the certificate
+# To sign your CSR with minikube CA
 openssl x509 -req \
   -in ${OUTPUT_DIR}/${USERNAME}.csr \
-  -CA ~/.minikube/ca.crt \
-  -CAkey ~/.minikube/ca.key \
+  -CA ${OUTPUT_DIR}/ca.crt \
+  -CAkey ${OUTPUT_DIR}/ca.key \
   -CAcreateserial \
   -out ${OUTPUT_DIR}/${USERNAME}.crt \
   -days 500
-cat ${OUTPUT_DIR}/${USERNAME}.crt
 
-# Check the content of the certificate
-openssl x509 -in ${OUTPUT_DIR}/${USERNAME}.crt -text -noout
+# Read X509 Certificate
+# Print Certificate Purpose
+openssl x509 -in ${OUTPUT_DIR}/${USERNAME}.crt -text -noout -purpose
