4dafbd9c955717348357d34ca49f5edbe7cd2a60
Harrison Van <harrison.van@shopback.com>
2019-11-27 15:13:59 +0700

    cleanup

--- 08-rbac-authorization/labs/03-playing-with-helm/test.sh
+++ 08-rbac-authorization/labs/03-playing-with-helm/test.sh
@@ -7,8 +7,8 @@ helm install stable/dokuwiki --namespace=test
 
 # We need to grant some extra permissions for harrison to access tiller
 kubectl config use-context minikube
-kubectl apply -f ./yaml/07-helm-tiller-access.yaml
-kubectl apply -f ./yaml/08-salme-use-tiller.yaml
+kubectl apply --filename 01-helm-tiller-access.yaml
+kubectl apply --filename 02-harrison-use-tiller.yaml
 
 # Try now
 kubectl config use-context harrison@minikube
@@ -34,11 +34,11 @@ helm install stable/dokuwiki
 # Let's fix this
 kubectl config use-context minikube
 kubectl create serviceaccount tiller-sa --namespace=kube-system
-kubectl apply -f yaml/09-tiller-clusterrolebinding.yaml
+kubectl apply --filename 04-tiller-clusterrolebinding.yaml
 
 # Redeploy helm
 # Update the tiller pod
 helm init --upgrade --service-account tiller-sa
 
 # Let's check if Tiller works now
-helm ls
+helm list
