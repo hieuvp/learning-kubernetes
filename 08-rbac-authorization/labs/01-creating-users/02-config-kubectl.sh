cbbcd6f9cb9be1034ba7bf5c3425455012d91c81
Harrison Van <harrison.van@shopback.com>
2019-11-28 12:09:58 +0700

    02-config-kubectl

--- 08-rbac-authorization/labs/01-creating-users/02-config-kubectl.sh
+++ 08-rbac-authorization/labs/01-creating-users/02-config-kubectl.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
-# To add in your local machine the new configuration
+declare -r MINIKUBE_IP="192.168.99.100"
+declare -r CERTIFICATE_DIR=".certificates"
+declare -r CERTIFICATE_USER="harrison"
 
-declare -r MINIKUBE_IP=192.168.99.100
-
-# Add new kubectl context
+######################################################
+# Add into your local machine the new configuration
+######################################################
 
 # Add the new cluster to kubectl
 kubectl config set-cluster minikube \
-  --certificate-authority=.certificates/ca.crt \
+  --certificate-authority=${CERTIFICATE_DIR}/ca.crt \
   --embed-certs=true \
   --server=https://${MINIKUBE_IP}:8443
 
 # Add the new credentials to kubectl
-kubectl config set-credentials harrison@minikube \
-  --client-certificate=.certificates/harrison.crt \
-  --client-key=.certificates/harrison.key \
+kubectl config set-credentials ${CERTIFICATE_USER}@minikube \
+  --client-certificate=${CERTIFICATE_DIR}/${CERTIFICATE_USER}.crt \
+  --client-key=${CERTIFICATE_DIR}/${CERTIFICATE_USER}.key \
   --embed-certs=true
 
 # Add the new context to kubectl
-kubectl config set-context harrison@minikube \
+kubectl config set-context ${CERTIFICATE_USER}@minikube \
   --cluster=minikube \
-  --user=harrison@minikube
+  --user=${CERTIFICATE_USER}@minikube
 
 # Change to the newly created context
-kubectl config use-context harrison@minikube
+kubectl config use-context ${CERTIFICATE_USER}@minikube
