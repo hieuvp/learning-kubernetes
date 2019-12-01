c856786872a13fa47b0ce1eae331e3453a05da4d
Harrison Van <harrison.van@shopback.com>
2019-12-01 17:16:47 +0700

    docs

--- 08-rbac-authorization/labs/01-creating-users/02-config-kubectl.sh
+++ 08-rbac-authorization/labs/01-creating-users/02-config-kubectl.sh
@@ -12,28 +12,36 @@ declare -r CERTIFICATE_USER="harrison"
 # Add into your local machine the new configuration
 #####################################################
 
+# --embed-certs=true
+# The certificates embedded as base64-encoded string in the kubeconfig file
+
 # Add the new cluster to kubectl
-# Set a cluster entry in kubeconfig
+# Set a "cluster" entry in kubeconfig
 kubectl config set-cluster ${CLUSTER_NAME} \
   --certificate-authority=${CERTIFICATE_DIR}/ca.crt \
   --embed-certs=true \
   --server=https://${CLUSTER_IP}:${CLUSTER_PORT}
 # embed-certs for the cluster entry in kubeconfig
+# certificate-authority-data
 
 # Add the new credentials to kubectl
-# Set a user entry in kubeconfig
+# Set a "user" entry in kubeconfig
 kubectl config set-credentials ${CERTIFICATE_USER}@${CLUSTER_NAME} \
   --client-certificate=${CERTIFICATE_DIR}/${CERTIFICATE_USER}.crt \
   --client-key=${CERTIFICATE_DIR}/${CERTIFICATE_USER}.key \
   --embed-certs=true
 # --embed-certs: Embed client cert/key for the user entry in kubeconfig
+# It will create the key client-key-data within the user entry of the kubeconfig file
+# and set the base64 encoding of dave.key as the value.
+# client-certificate-data
+# client-key-data
 
 # Add the new context to kubectl
-# Set a context entry in kubeconfig
+# Set a "context" entry in kubeconfig
 kubectl config set-context ${CERTIFICATE_USER}@${CLUSTER_NAME} \
   --cluster=${CLUSTER_NAME} \
   --user=${CERTIFICATE_USER}@${CLUSTER_NAME}
 
 # Change to the newly created context
-# Set the current-context in a kubeconfig file
+# Set the "current-context" in kubeconfig
 kubectl config use-context ${CERTIFICATE_USER}@${CLUSTER_NAME}
