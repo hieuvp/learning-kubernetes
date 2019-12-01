b7f207ed36ff7e409b569bb5aeaf44ac9b42baf9
Harrison Van <harrison.van@shopback.com>
2019-12-01 21:50:04 +0700

    test

--- 08-rbac-authorization/labs/03-playing-with-helm/test.sh
+++ 08-rbac-authorization/labs/03-playing-with-helm/test.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 set -x pipefail
 
-# Try deploying a dokuwiki chart
 helm list
+
 helm install stable/dokuwiki --namespace=test
 kubectl get pods --namespace=test --watch
+
+helm list
 
 helm install stable/dokuwiki
 kubectl run --image=bitnami/dokuwiki dokuwiki
 kubectl get pods
 
+helm list
+
 helm install stable/dokuwiki --namespace=kube-system
 
-# Let's try now
-helm list
-helm install stable/dokuwiki
-
-# Let's check if Tiller works now
 helm list
