36ab665e46a25aa64009a2a14fcbc25e4be798f8
Harrison Van <harrison.van@shopback.com>
2019-12-02 10:47:24 +0700

    test

--- 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
+++ 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -x pipefail
 
-kubectl get pods
 kubectl get pods --namespace=test
 kubectl get pods --namespace=kube-system
+kubectl get pods
 
 timeout 3s kubectl get pods --watch --namespace=test
 
-kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2
 kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2 --namespace=test
+kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2
 
 kubectl expose deployment nginx --type=NodePort --port=80 --namespace=test
 
-kubectl get services
 kubectl get services --namespace=test
+kubectl get services
