c77af5bbbe124ca5c1e67b94e72b66432eafa646
Harrison Van <harrison.van@shopback.com>
2019-12-02 10:04:38 +0700

    deprecated

--- 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
+++ 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
@@ -7,8 +7,8 @@ kubectl get pods --namespace=kube-system
 
 timeout 3s kubectl get pods --watch --namespace=test
 
-kubectl run nginx --image=nginx --replicas=2
-kubectl run nginx --image=nginx --replicas=2 --namespace=test
+kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2
+kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2 --namespace=test
 
 kubectl expose deployment nginx --type=NodePort --port=80 --namespace=test
 
