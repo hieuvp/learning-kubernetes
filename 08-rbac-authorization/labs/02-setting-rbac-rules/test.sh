2201e3d96d907904cf4677068d6307dd1233b4df
Harrison Van <harrison.van@shopback.com>
2019-12-02 11:01:54 +0700

    test

--- 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
+++ 08-rbac-authorization/labs/02-setting-rbac-rules/test.sh
@@ -10,7 +10,7 @@ timeout 3s kubectl get pods --watch --namespace=test
 kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2 --namespace=test
 kubectl run nginx --generator=run-pod/v1 --image=nginx --replicas=2
 
-kubectl expose deployment nginx --type=NodePort --port=80 --namespace=test
+kubectl expose pod nginx --type=NodePort --port=80 --namespace=test
 
 kubectl get services --namespace=test
 kubectl get services
