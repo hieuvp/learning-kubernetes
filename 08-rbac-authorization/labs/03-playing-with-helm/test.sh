63a84df5dca2d1176cd86a39da0a0e13d568f1ae
Harrison Van <harrison.van@shopback.com>
2019-12-02 11:53:34 +0700

    test.sh

--- 08-rbac-authorization/labs/03-playing-with-helm/test.sh
+++ 08-rbac-authorization/labs/03-playing-with-helm/test.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 set -x pipefail
 
-helm list
-
+# Namespace "test"
 helm install stable/dokuwiki --namespace=test
+helm list
 timeout 5s kubectl get pods --namespace=test --watch
 
-helm list
-
+# Namespace "default"
 helm install stable/dokuwiki
-kubectl run --image=bitnami/dokuwiki dokuwiki
+helm list
+kubectl run dokuwiki --generator=run-pod/v1 --image=bitnami/dokuwiki
 kubectl get pods
 
-helm list
-
+# Namespace "kube-system"
 helm install stable/dokuwiki --namespace=kube-system
-
 helm list
