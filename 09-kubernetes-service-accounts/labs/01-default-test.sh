796999f9c1b5a1688861dc74e079aabe22344252
Harrison Van <harrison.van@shopback.com>
2019-12-07 14:45:29 +0700

    docs

--- 09-kubernetes-service-accounts/labs/01-default-test.sh
+++ 09-kubernetes-service-accounts/labs/01-default-test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -x pipefail
 
-# Anonymously call of the API Server
-# Get information from the API Server without authentication
+# Anonymous call of the API Server without authentication
 curl https://kubernetes/api/v1 --insecure
 
-# Call using the ServiceAccount token
+# Using the ServiceAccount token
 TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
 curl https://kubernetes/api/v1 --insecure \
   --header "Authorization: Bearer ${TOKEN}"
